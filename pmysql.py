3fee039237e0cd90e78f265cd9d70e2772f1b5c1
yejunhui <1589082159@qq.com>
2020-03-09 16:25:58 +0800

    '3'

--- pmysql.py
+++ pmysql.py
@@ -1,9 +1,14 @@
 #本文件需要用到pymysql
 import pymysql
 
+h1='w.rdc.sae.sina.com.cn'
+user1='xnl0zzw55m'
+password1='2w051l1wy22y12i5xm5ly3mhyi03jw35zy402543'
+db1='app_gold'
+
 class pmysql:
     def __init__(self,h='localhost',user='user',password='123456',db='uransData'):
-        self.conn = pymysql.connect(host=h,user=user,password=password,db=db)
+        self.conn = pymysql.connect(host=h1,user=user1,password=password1,db=db1)
         self.conn.autocommit(True)
         self.cur = self.conn.cursor()
 
