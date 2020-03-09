b1dedf03022431a50a84f918b115313575315f4f
yejunhui <1589082159@qq.com>
2020-03-09 15:16:48 +0800

    2

--- pmysql.py
+++ pmysql.py
@@ -2,7 +2,7 @@
 import pymysql
 
 class pmysql:
-    def __init__(self,h='localhost',user='user',password='123456',db='uransData'):
+    def __init__(self,h='w.rdc.sae.sina.com.cn',user='xnl0zzw55m',password='2w051l1wy22y12i5xm5ly3mhyi03jw35zy402543',db='app_gold'):
         self.conn = pymysql.connect(host=h,user=user,password=password,db=db)
         self.conn.autocommit(True)
         self.cur = self.conn.cursor()
